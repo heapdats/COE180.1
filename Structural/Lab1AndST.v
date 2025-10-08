6a474c1e7f16d37a1e683507d8fccc536ab9318c
heapdats <mmtobillo@gmail.com>
2025-10-08 18:08:02 +0800

    Update Lab1AndST.v

--- Structural/Lab1AndST.v
+++ Structural/Lab1AndST.v
@@ -1,7 +1,27 @@
-module and_gate_dt (
-	input a,
-	input b,
-	output y
+module and_gate_st (
+    input a,
+    input b,
+    output y
 );
-	assign y = a & b;
+
+    wire not_a, not_b;
+
+    // Invert the inputs
+    not_gate_df not_gate_a (
+        .a(a),
+        .y(not_a)
+    );
+
+    not_gate_df not_gate_b (
+        .a(b),
+        .y(not_b)
+    );
+
+    // Pass the inverted inputs through a NOR gate
+    nor_gate_df nor1 (
+        .a(not_a),
+        .b(not_b),
+        .y(y)
+    );
+
 endmodule
